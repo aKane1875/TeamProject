a6f8dba06774a990a2df00a24e59508d390e2945
Allan Kane <allanhfc@yahoo.co.uk>
2022-01-26 16:53:36 +0000

    working on accountScreen and firebase

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,168 +1,206 @@
-import React, { useState } from 'react';
-import { Text, StyleSheet, View, Image, TextInput, TouchableOpacity, ScrollView } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 //import { firebase } from '../Firebase/firebase';
-import FormError from '../Components/FormError';
-import FormSuccess from '../Components/FormSuccess';
-import { Ionicons } from '@expo/vector-icons';
+import FormError from "../Components/FormError";
+import FormSuccess from "../Components/FormSuccess";
+import { Ionicons } from "@expo/vector-icons";
 
-import { auth } from '../Firebase/firebase';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from "../Firebase/firebase";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  updateProfile,
+} from "firebase/auth";
 
 const SignUp = ({ navigation }) => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [errMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [displayFormErr, setDisplayFormErr] = useState(false);
 
-    const [fullName, setFullName] = useState('');
-    const [email, setEmail] = useState('');
-    const [errMessage, setErrorMessage] = useState('');
-    const [successMessage, setSuccessMessage] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
-    const [password, setPassword] = useState();
-    const [confirmPassword, setConfirmPassword] = useState();
-    const [displayFormErr, setDisplayFormErr] = useState(false);
+  function fullNameChange(value) {
+    setFullName(value);
+  }
 
-    function fullNameChange(value) {
-        setFullName(value);
+  function navigate() {
+    navigation.navigate("signIn");
+  }
+
+  function createUser() {
+    setIsLoading(true);
+    // firebase.auth().createUserWithEmailAndPassword(email, password)
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        updateProfile(auth.currentUser, {
+          displayName: fullName,
+        }).then(() => {});
+        setIsLoading(false);
+        setSuccessMessage("Your account has been created");
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setErrorMessage(err.message);
+        setDisplayFormErr(true);
+      });
+  }
+
+  const validateForm = () => {
+    var form_inputs = [fullName, email, password, confirmPassword];
+    var passwords_match = password == confirmPassword;
+
+    if (form_inputs.includes("") || form_inputs.includes(undefined)) {
+      setErrorMessage("Please fill in all fields");
+      return setDisplayFormErr(true);
     }
 
-    function navigate() {
-        navigation.navigate('signIn');
+    if (!passwords_match) {
+      setErrorMessage("Passwords do not match");
+      return setDisplayFormErr(true);
     }
 
-    function createUser() {
-        setIsLoading(true);
-        // firebase.auth().createUserWithEmailAndPassword(email, password)
-        createUserWithEmailAndPassword(auth, email, password)
-            .then(() => {
-                setIsLoading(false);
-                setSuccessMessage("Your account has been created");
-            })
-            .catch((err) => {
-                setIsLoading(false);
-                setErrorMessage(err.message);
-                setDisplayFormErr(true);
-            })
+    if (passwords_match) return createUser();
+  };
 
-    }
-
-    const validateForm = () => {
-        var form_inputs = [fullName, email, password, confirmPassword];
-        var passwords_match = password == confirmPassword;
-
-        if (form_inputs.includes('') || form_inputs.includes(undefined)) {
-            setErrorMessage("Please fill in all fields");
-            return setDisplayFormErr(true);
-        }
-
-        if (!passwords_match) {
-            setErrorMessage("Passwords do not match");
-            return setDisplayFormErr(true);
-        }
-
-        if (passwords_match) return createUser();
-    }
-
-    return (
-        <View style={styles.container}>
-
-            <ScrollView style={styles.BottomView}>
-                <Ionicons onPress={navigate} style={styles.Icon} name="arrow-back-circle-outline" color={"#fff"} size={60} />
-                <Text style={styles.Heading}>
-                    Create your{'\n'}
-                    account
-                </Text>
-                <View style={styles.FormView}>
-                    <TextInput onChangeText={fullNameChange} value={fullName} placeholder={"Full name*"} placeholderTextColor={"#fff"} style={styles.TextInput} />
-                    <TextInput onChangeText={(val) => setEmail(val)} placeholder={"Email address*"} value={email} placeholderTextColor={"#fff"} style={styles.TextInput} />
-                    <TextInput onChangeText={(val) => setPassword(val)} placeholder={"Password*"} value={password} secureTextEntry={true} placeholderTextColor={"#fff"} style={styles.TextInput} />
-                    <TextInput onChangeText={(val) => setConfirmPassword(val)} placeholder={"Confirm Password*"} value={confirmPassword} secureTextEntry={true} placeholderTextColor={"#fff"} style={styles.TextInput} />
-                    <TouchableOpacity onPress={validateForm} style={styles.Button}>
-                        <Text style={styles.ButtonText}>Sign up</Text>
-                    </TouchableOpacity>
-                </View>
-
-            </ScrollView>
-            {displayFormErr == true ?
-                <FormError hideErrOverlay={setDisplayFormErr} err={errMessage} />
-                :
-                null
-            }
-
-            {isLoading == true ?
-                <FormSuccess />
-                :
-                successMessage == "Your account has been created" ?
-                    <FormSuccess successMessage={successMessage} close={setSuccessMessage} />
-                    :
-                    null
-            }
-
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.BottomView}>
+        <Ionicons
+          onPress={navigate}
+          style={styles.Icon}
+          name="arrow-back-circle-outline"
+          color={"#fff"}
+          size={60}
+        />
+        <Text style={styles.Heading}>
+          Create your{"\n"}
+          account
+        </Text>
+        <View style={styles.FormView}>
+          <TextInput
+            onChangeText={(val) => setFullName(val)}
+            value={fullName}
+            placeholder={"Full name*"}
+            placeholderTextColor={"#fff"}
+            style={styles.TextInput}
+          />
+          <TextInput
+            onChangeText={(val) => setEmail(val)}
+            placeholder={"Email address*"}
+            value={email}
+            placeholderTextColor={"#fff"}
+            style={styles.TextInput}
+          />
+          <TextInput
+            onChangeText={(val) => setPassword(val)}
+            placeholder={"Password*"}
+            value={password}
+            secureTextEntry={true}
+            placeholderTextColor={"#fff"}
+            style={styles.TextInput}
+          />
+          <TextInput
+            onChangeText={(val) => setConfirmPassword(val)}
+            placeholder={"Confirm Password*"}
+            value={confirmPassword}
+            secureTextEntry={true}
+            placeholderTextColor={"#fff"}
+            style={styles.TextInput}
+          />
+          <TouchableOpacity onPress={validateForm} style={styles.Button}>
+            <Text style={styles.ButtonText}>Sign up</Text>
+          </TouchableOpacity>
         </View>
-    )
-}
+      </ScrollView>
+      {displayFormErr == true ? (
+        <FormError hideErrOverlay={setDisplayFormErr} err={errMessage} />
+      ) : null}
+
+      {isLoading == true ? (
+        <FormSuccess />
+      ) : successMessage == "Your account has been created" ? (
+        <FormSuccess
+          successMessage={successMessage}
+          close={setSuccessMessage}
+        />
+      ) : null}
+    </View>
+  );
+};
 
 export default SignUp;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    BottomView: {
-        width: '100%',
-        backgroundColor: 'tomato'
-    },
-    Heading: {
-        color: '#fff',
-        fontSize: 40,
-        fontWeight: 'bold',
-        marginLeft: 30,
-        marginTop: 5
-    },
-    FormView: {
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        marginTop: 10
-    },
-    TextInput: {
-        width: '90%',
-        borderWidth: 1,
-        borderColor: '#fff',
-        height: 52,
-        borderRadius: 10,
-        paddingLeft: 5,
-        marginTop: 20,
-        color: '#fff'
-    },
-    Button: {
-        width: '90%',
-        color: '#000',
-        height: 52,
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        marginTop: 20,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    ButtonText: {
-        fontWeight: 'bold',
-        fontSize: 18
-    },
-    SignUpText: {
-        color: 'gray',
-    },
-    TextButton: {
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        marginTop: 20
-    },
-    Icon: {
-        marginLeft: 5,
-        marginTop: 30
-    }
-
-
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  BottomView: {
+    width: "100%",
+    backgroundColor: "tomato",
+  },
+  Heading: {
+    color: "#fff",
+    fontSize: 40,
+    fontWeight: "bold",
+    marginLeft: 30,
+    marginTop: 5,
+  },
+  FormView: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  TextInput: {
+    width: "90%",
+    borderWidth: 1,
+    borderColor: "#fff",
+    height: 52,
+    borderRadius: 10,
+    paddingLeft: 5,
+    marginTop: 20,
+    color: "#fff",
+  },
+  Button: {
+    width: "90%",
+    color: "#000",
+    height: 52,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    marginTop: 20,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  ButtonText: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  SignUpText: {
+    color: "gray",
+  },
+  TextButton: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  Icon: {
+    marginLeft: 5,
+    marginTop: 30,
+  },
+});
--- screens/signedIn/TestToolScreen.js
+++ screens/signedIn/TestToolScreen.js
@@ -1,37 +1,41 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
 //import { firebase } from '../../Firebase/firebase';
 
-import { collection, getDocs, doc, setDoc, getDoc } from 'firebase/firestore/lite';
-import { auth, db } from '../../Firebase/firebase';
+import {
+  collection,
+  getDocs,
+  doc,
+  setDoc,
+  getDoc,
+} from "firebase/firestore/lite";
+import { auth, db } from "../../Firebase/firebase";
 
 const TestToolScreen = () => {
+  const GetSingleItem = async () => {
+    const docRef = doc(db, "users", auth.currentUser.uid);
+    const docSnap = await getDoc(docRef);
 
-    const GetSingleItem = async () => {
-      const docRef = doc(db, "users", auth.currentUser.uid);
-      const docSnap = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-          // console.log("Document data:", docSnap.data().city_name);
-          console.log("Document data:", docSnap.data());
-        } else {
-          // doc.data() will be undefined in this case
-          console.log("No such document!");
-        }
+    if (docSnap.exists()) {
+      // console.log("Document data:", docSnap.data().city_name);
+      console.log("Document data:", docSnap.data());
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
   };
 
   const GetAllData = async () => {
-      const citiesCol = collection(db, 'users');
-      const citySnapshot = await getDocs(citiesCol);
-      const cityList = citySnapshot.docs.map(doc => doc.data());
-      console.log(cityList);
+    const citiesCol = collection(db, "users");
+    const citySnapshot = await getDocs(citiesCol);
+    const cityList = citySnapshot.docs.map((doc) => doc.data());
+    console.log(cityList);
   };
 
-
   const AddData = async () => {
-    const city = "London";
-    await setDoc(doc(db, "users", auth.currentUser.uid), {
-        city_name: city,
+    const city = "Glasgow";
+    await setDoc(doc(db, "gameboard", "leeds-board"), {
+      city_name: city,
     });
     //Add a new document in collection "users"
   };
@@ -40,21 +44,15 @@ const TestToolScreen = () => {
     <View style={styles.container}>
       <Text>Tools</Text>
 
-      <TouchableOpacity
-        onPress={GetSingleItem}
-        style={styles.button}>
+      <TouchableOpacity onPress={GetSingleItem} style={styles.button}>
         <Text style={styles.buttonText}>GET ONE BIT OF DATA</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={GetAllData}
-        style={styles.button}>
+      <TouchableOpacity onPress={GetAllData} style={styles.button}>
         <Text style={styles.buttonText}>GET DATA FROM FIREBASE</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={AddData}
-        style={styles.button}>
+      <TouchableOpacity onPress={AddData} style={styles.button}>
         <Text style={styles.buttonText}>ADD DATA TO FIREBASE</Text>
       </TouchableOpacity>
     </View>
@@ -66,21 +64,20 @@ export default TestToolScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   button: {
-    backgroundColor: 'tomato',
-    width: '60%',
+    backgroundColor: "tomato",
+    width: "60%",
     padding: 15,
     borderRadius: 10,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 40,
   },
   buttonText: {
-    color: 'white',
-    fontWeight: '700',
+    color: "white",
+    fontWeight: "700",
     fontSize: 16,
   },
 });
-
