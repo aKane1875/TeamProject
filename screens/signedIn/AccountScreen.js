b2cb53215ac70f446e146804afc2ca603223eb57
Iznara <ryanmcguire1991@gmail.com>
2022-01-28 12:46:23 +0000

    added profile picture to account screen

--- screens/signedIn/AccountScreen.js
+++ screens/signedIn/AccountScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React, { useEffect, useState } from "react";
 // import { firebase } from '../../Firebase/firebase';
 import { auth, db } from "../../Firebase/firebase";
@@ -12,14 +12,14 @@ const AccountScreen = () => {
   const [user, setUser] = useState({});
 
   useEffect(() => {
-    
+
     const GetSingleUser = async () => {
       const docRef = doc(db, "user", auth.currentUser.uid);
       const docSnap = await getDoc(docRef);
-  
+
       if (docSnap.exists()) {
         // console.log("Document data:", docSnap.data().city_name);
-        console.log("Document data:", docSnap.data());
+        console.log("Document data getUser:", docSnap.data());
         setUser(docSnap.data());
         // console.log(user);
       } else {
@@ -33,80 +33,99 @@ const AccountScreen = () => {
 
 
 
-	// logs e-mail address of user, also has a displayName key (username for our app that people can create when they sign up??)
-	// console.log(firebase.auth());
+  // logs e-mail address of user, also has a displayName key (username for our app that people can create when they sign up??)
+  // console.log(firebase.auth());
 
-	// causes error when I restarted the app, need to be signed in before visiting, shoud be an easy fix
-	// const fullname = auth.currentUser.displayName;
-	// console.log(auth.currentUser);
+  // causes error when I restarted the app, need to be signed in before visiting, shoud be an easy fix
+  // const fullname = auth.currentUser.displayName;
+  // console.log(auth.currentUser);
 
-	const handleSignOut = () => {
-		// firebase.auth().signOut();
-		signOut(auth);
-	};
+  const handleSignOut = () => {
+    // firebase.auth().signOut();
+    signOut(auth);
+  };
 
-	const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
 
-	return (
-		<View>
-			<Text>{user.fullname}ACCOUNT DETAILS</Text>
-			<Text>PROFILE PIC HERE</Text>
-			<Text>TOTAL HEXAGONS: </Text>
-			<Text>TOTAL WINS: </Text>
+  return (
+    <View>
+      <Text>{user.fullname}ACCOUNT DETAILS</Text>
+      <Text>PROFILE PIC HERE</Text>
+
+      <View style={styles.container}>
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: user.picture,
+        }}
+      />
+    </View>
+
+
+      <Text>TOTAL HEXAGONS: </Text>
+      <Text>TOTAL WINS: </Text>
 
       {/* <TouchableOpacity onPress={GetSingleUser} style={styles.button}>
 				<Text style={styles.buttonText}>Get user data</Text>
 			</TouchableOpacity> */}
 
-			<TouchableOpacity onPress={handleSignOut} style={styles.button}>
-				<Text style={styles.buttonText}>Sign out</Text>
-			</TouchableOpacity>
-		</View>
-	);
+      <TouchableOpacity onPress={handleSignOut} style={styles.button}>
+        <Text style={styles.buttonText}>Sign out</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default AccountScreen;
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	buttonContainer: {
-		width: "60%",
-		justifyContent: "center",
-		alignItems: "center",
-		marginTop: 40,
-	},
-	button: {
-		backgroundColor: "tomato",
-		width: "100%",
-		padding: 15,
-		borderRadius: 10,
-		alignItems: "center",
-	},
-	buttonOutline: {
-		backgroundColor: "white",
-		marginTop: 5,
-		borderColor: "#0782F9",
-		borderWidth: 2,
-	},
-	buttonText: {
-		color: "white",
-		fontWeight: "700",
-		fontSize: 16,
-	},
-	buttonOutlineText: {
-		color: "#0782F9",
-		fontWeight: "700",
-		fontSize: 16,
-	},
-	centeredView: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-		marginTop: 22,
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: "tomato",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#0782F9",
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: "#0782F9",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
 });
