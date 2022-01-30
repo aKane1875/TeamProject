d38271317b039b136a757d851bce86ad9e1d7dac
Iznara <ryanmcguire1991@gmail.com>
2022-01-30 20:21:58 +0000

    correcting haxagon spellings

--- screens/signedIn/TestToolScreen.js
+++ screens/signedIn/TestToolScreen.js
@@ -1,53 +1,78 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
-//import { firebase } from '../../Firebase/firebase';
-
-import { collection, getDocs, doc, setDoc, getDoc } from "firebase/firestore";
+import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View, Share, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+// import ProgressBar from 'react-native-progress/Bar';
 import { auth, db } from "../../Firebase/firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 const TestToolScreen = () => {
-	const GetSingleItem = async () => {
-		const docRef = doc(db, "gameboard", "test-board");
-		const docSnap = await getDoc(docRef);
+	const [inputValue, setInputValue] = useState('');
+	const [user, setUser] = useState({});
 
-		if (docSnap.exists()) {
-			// console.log("Document data:", docSnap.data().city_name);
-			console.log("Document data:", docSnap.data().board[0].col);
-		} else {
-			// doc.data() will be undefined in this case
-			console.log("No such document!");
-		}
+	useEffect(() => {
+		const GetDeets = async () => {
+			const docRef = doc(db, "user", auth.currentUser.uid);
+			const docSnap = await getDoc(docRef);
+			if (docSnap.exists()) {
+				setUser(docSnap.data());
+				console.log(docSnap.data());
+			} else {
+				console.log("No such document!");
+			}
+		};
+		GetDeets();
+	}, []);
+
+
+	//https://docs.expo.dev/versions/latest/sdk/sharing/
+	const shareMessage = () => {
+		//Here is the Share API
+		Share.share({
+			message: inputValue.toString(),
+		})
+			//after successful share return result
+			.then((result) => console.log(result))
+			//If any thing goes wrong it comes here
+			.catch((errorMsg) => console.log(errorMsg));
 	};
 
-	const GetAllData = async () => {
-		const citiesCol = collection(db, "gameboard");
-		const citySnapshot = await getDocs(citiesCol);
-		const cityList = citySnapshot.docs.map((doc) => doc.data());
-		console.log(cityList);
-	};
 
-	const AddData = async () => {
-		const city = "Glasgow";
-		await setDoc(doc(db, "gameboard", "leeds-board"), {
-			city_name: city,
-		});
-		//Add a new document in collection "users"
-	};
 
 	return (
 		<View style={styles.container}>
-			<Text>Tools</Text>
-			<TouchableOpacity onPress={GetSingleItem} style={styles.button}>
-				<Text style={styles.buttonText}>GET ONE BIT OF DATA</Text>
-			</TouchableOpacity>
+				<TextInput
+					value={inputValue}
+					onChangeText={
+						(inputValue) => setInputValue(inputValue)
+					}
+					placeholder={'Enter Text to Share'}
+					style={styles.textInput}
+				/>
+				<TouchableOpacity
+					activeOpacity={0.7}
+					style={styles.button}
+					onPress={shareMessage}>
+					<Text style={styles.buttonText}>
+						Share Input Text
+					</Text>
+				</TouchableOpacity>
+{/* 
+				<Text>Collect 10 Hexagons:</Text>
+				<ProgressBar progress={user.total_hexagons / 10} width={200} height={10} color="rgba(0, 122, 255, 1)" /> */}
 
-			<TouchableOpacity onPress={GetAllData} style={styles.button}>
-				<Text style={styles.buttonText}>GET DATA FROM FIREBASE</Text>
-			</TouchableOpacity>
-
-			<TouchableOpacity onPress={AddData} style={styles.button}>
-				<Text style={styles.buttonText}>ADD DATA TO FIREBASE</Text>
-			</TouchableOpacity>
+				{/* <Text>Collect 10 Hexagons:</Text>
+				<ProgressBar progress={user.total_hexagons / 10} width={200} height={10} color={user.fav_colour} />
+				<Text>Collect 100 Hexagons:</Text>
+				<ProgressBar progress={user.total_hexagons / 100} width={200} height={10} color={user.fav_colour} />
+				<Text>Collect 1000 Hexagons:</Text>
+				<ProgressBar progress={user.total_hexagons / 1000} width={200} height={10} color={user.fav_colour} />
+				<Text>Run 10 Kilometers:</Text>
+				<ProgressBar progress={user.total_distance / 10} width={200} height={10} color={user.fav_colour} />
+				<Text>Run 100 Kilometers:</Text>
+				<ProgressBar progress={user.total_distance / 100} width={200} height={10} color={user.fav_colour} />
+				<Text>Run for 10 minutes:</Text>
+				<ProgressBar progress={user.total_playtime / 10} width={200} height={10} color={user.fav_colour} />
+				<Text>Run for 100 minutes:</Text>
+				<ProgressBar progress={user.total_playtime / 100} width={200} height={10} color={user.fav_colour} /> */}
 		</View>
 	);
 };
@@ -72,5 +97,16 @@ const styles = StyleSheet.create({
 		color: "white",
 		fontWeight: "700",
 		fontSize: 16,
+	},
+	titleTextsmall: {
+		marginVertical: 8,
+		fontSize: 16,
+	},
+	textInput: {
+		height: 40,
+		borderColor: 'gray',
+		borderWidth: 1,
+		width: '90%',
+		paddingHorizontal: 10,
 	},
 });
