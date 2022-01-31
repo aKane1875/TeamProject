44c88364db58c7ab1814125598c0cd8806c664ba
Allan Kane <allanhfc@yahoo.co.uk>
2022-01-31 12:34:42 +0000

    experimenting with grid size

--- utils/helpers.js
+++ utils/helpers.js
@@ -8,125 +8,125 @@ import { doc, setDoc, onSnapshot, getDoc } from "firebase/firestore";
 //GETS POINTS STORED BY THE TASK MANAGER IN LOCAL AsycnStorage, ADDS IT NEW
 // POINT TO IT AND THEN PPUTS IT BACK IN STORAGE
 export const updateTrackerArray = async (newValue) => {
-	try {
-		let newArray;
-		let jsonValue = await AsyncStorage.getItem("trackerArray");
-		if (jsonValue) {
-			const parsedArray = JSON.parse(jsonValue);
-			newArray = [...parsedArray, newValue];
-		} else {
-			newArray = [newValue];
-		}
-		const newJsonValue = JSON.stringify(newArray);
-		await AsyncStorage.setItem("trackerArray", newJsonValue);
-	} catch (e) {
-		console.log("update tracker array error", e);
-	}
+  try {
+    let newArray;
+    let jsonValue = await AsyncStorage.getItem("trackerArray");
+    if (jsonValue) {
+      const parsedArray = JSON.parse(jsonValue);
+      newArray = [...parsedArray, newValue];
+    } else {
+      newArray = [newValue];
+    }
+    const newJsonValue = JSON.stringify(newArray);
+    await AsyncStorage.setItem("trackerArray", newJsonValue);
+  } catch (e) {
+    console.log("update tracker array error", e);
+  }
 };
 
 //GENERATE THE GAME BOARD
 export const createBoard = (_lon, _lat) => {
-	console.log("board created");
-	const board_size = 0.02;
-	const board_h = board_size * 2; //can change this 2 value to adjust for a more square board
-	const bbox = [
-		_lon - board_h,
-		_lat - board_size,
-		_lon + board_h,
-		_lat + board_size,
-	];
+  console.log("board created");
+  const board_size = 0.02;
+  const board_h = board_size * 2; //can change this 2 value to adjust for a more square board
+  const bbox = [
+    _lon - board_h,
+    _lat - board_size,
+    _lon + board_h,
+    _lat + board_size,
+  ];
 
-	//CREATE HEX GRID
-	const cellSide = 0.12; //higher number means fewer, bigger hexes. porbably improves performance
-	const options = { units: "kilometres" };
-	const grid = hexGrid(bbox, cellSide, options);
+  //CREATE HEX GRID
+  const cellSide = 0.12; //higher number means fewer, bigger hexes. porbably improves performance
+  const options = { units: "kilometres" };
+  const grid = hexGrid(bbox, cellSide, options);
 
-	//EXTRACT JUST COORDINATES FROM HEX DATA - REMOVE ALL THE GEOJSON STUFF
-	const board = grid.features.map((feature) => feature.geometry.coordinates[0]);
+  //EXTRACT JUST COORDINATES FROM HEX DATA - REMOVE ALL THE GEOJSON STUFF
+  const board = grid.features.map((feature) => feature.geometry.coordinates[0]);
 
-	//CONVERT THE ARRAYS TO OBJECTS THAT MAPVIEW CAN READ
-	board.forEach((poly, index) => {
-		const _coords = poly.map((coordArr) => {
-			//creates an array of coord objects for each point of the hex
-			return {
-				longitude: coordArr[0],
-				latitude: coordArr[1],
-			};
-		});
+  //CONVERT THE ARRAYS TO OBJECTS THAT MAPVIEW CAN READ
+  board.forEach((poly, index) => {
+    const _coords = poly.map((coordArr) => {
+      //creates an array of coord objects for each point of the hex
+      return {
+        longitude: coordArr[0],
+        latitude: coordArr[1],
+      };
+    });
 
-		board[index] = {
-			//this is the object for each hex, includes the coords arry above as a proprty. Add any properties here that each will have, eg owner, team, etc
-			current_owner: null,
-			col: "rgba(0, 89, 255, 0.4)", //blue
-			coords: _coords,
-		};
-	});
-	AddData(board);
-	return board;
+    board[index] = {
+      //this is the object for each hex, includes the coords arry above as a proprty. Add any properties here that each will have, eg owner, team, etc
+      current_owner: null,
+      col: "rgba(0, 89, 255, 0.4)", //blue
+      coords: _coords,
+    };
+  });
+  AddData(board);
+  return board;
 };
 
 export const GetColour = async () => {
-	const docRef = doc(db, "user", auth.currentUser.uid);
-	const docSnap = await getDoc(docRef);
-	if (docSnap.exists()) {
-		console.log("get colour result: ", docSnap.data().fav_colour);
-		globalColour = docSnap.data().fav_colour;
-	} else {
-		console.log("No such user!");
-	}
+  const docRef = doc(db, "user", auth.currentUser.uid);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    console.log("get colour result: ", docSnap.data().fav_colour);
+    globalColour = docSnap.data().fav_colour;
+  } else {
+    console.log("No such user!");
+  }
 };
 
 export const AddListener = (setHexBoard) => {
-	const unsub = onSnapshot(doc(db, "gameboard", board_name), (doc) => {
-		setHexBoard(doc.data().board);
-		globalHexBoard = doc.data().board;
-		// console.log("Current data: ", doc.data().board[0].col);
-		const source = doc.metadata.hasPendingWrites ? "Local" : "Server";
-		console.log(source, " data: ", doc.data().board[0].col);
-	});
+  const unsub = onSnapshot(doc(db, "gameboard", board_name), (doc) => {
+    setHexBoard(doc.data().board);
+    globalHexBoard = doc.data().board;
+    // console.log("Current data: ", doc.data().board[0].col);
+    const source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+    console.log(source, " data: ", doc.data().board[0].col);
+  });
 };
 
 export const AddData = async (board) => {
-	await setDoc(doc(db, "gameboard", board_name), { board });
-	//Add a new document in collection "users"
+  await setDoc(doc(db, "gameboard", board_name), { board });
+  //Add a new document in collection "users"
 };
 
 //USE THIS IS CHECK IF USERS POSITION OR WHOLE PATH IS INSIDE ANY OF THE HEXES.
 export const checkPathIsInPolys = (pointsArray, hexArray, setHexBoard) => {
-	const newArray = [...hexArray];
+  const newArray = [...hexArray];
 
-	pointsArray.forEach((point) => {
-		for (const hex of newArray) {
-			if (isPointInPolygon(point, hex.coords)) {
-				//put stuff here if you want it to happen if a point is inside a hex. Can refer to the hex directly using "hex"
-				hex.col = "rgba(235, 210, 52, 0.3)"; //yellow
-				break;
-			}
-		}
-	});
-	setHexBoard(newArray);
+  pointsArray.forEach((point) => {
+    for (const hex of newArray) {
+      if (isPointInPolygon(point, hex.coords)) {
+        //put stuff here if you want it to happen if a point is inside a hex. Can refer to the hex directly using "hex"
+        hex.col = "rgba(235, 210, 52, 0.3)"; //yellow
+        break;
+      }
+    }
+  });
+  setHexBoard(newArray);
 };
 
 //FIND USERS CURRENT POSITION ONCE. MIGHT NOT NEED NOW USING BACKGROUND TRACKER?
 export const findUser = async () => {
-	const { status } = await Location.requestForegroundPermissionsAsync();
-	if (status !== "granted") {
-		console.log("Permission to access location was denied");
-		return;
-	} else {
-		console.log("Permission to access location granted");
-	}
+  const { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== "granted") {
+    console.log("Permission to access location was denied");
+    return;
+  } else {
+    console.log("Permission to access location granted");
+  }
 
-	const loc = await Location.getCurrentPositionAsync({
-		accuracy: Location.Accuracy.Balanced,
-		enableHighAccuracy: true,
-		timeInterval: 5000,
-		distanceInterval: 0,
-	});
+  const loc = await Location.getCurrentPositionAsync({
+    accuracy: Location.Accuracy.Balanced,
+    enableHighAccuracy: true,
+    timeInterval: 5000,
+    distanceInterval: 0,
+  });
 
-	setUserLoc({
-		//does this function need to be a paramater?
-		latitude: loc.coords.latitude,
-		longitude: loc.coords.longitude,
-	});
+  setUserLoc({
+    //does this function need to be a paramater?
+    latitude: loc.coords.latitude,
+    longitude: loc.coords.longitude,
+  });
 };
